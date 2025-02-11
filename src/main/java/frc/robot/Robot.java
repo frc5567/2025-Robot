5852ba1abf2181405507f833d41ca64cfbf265d6
coverbeek <chris@chrisoverbeek.com>
2025-02-11 17:44:13 -0500

    Changes to support SysId

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.SignalLogger;
 import com.ctre.phoenix6.Utils;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -71,13 +72,16 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    SignalLogger.start();
   }
 
   @Override
   public void teleopPeriodic() {}
 
   @Override
-  public void teleopExit() {}
+  public void teleopExit() {
+    SignalLogger.stop();
+  }
 
   @Override
   public void testInit() {
--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -104,8 +104,12 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
               this));
 
   /* The SysId routine to test */
-  private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
+  // private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineTranslation;
+  // private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineSteer;
 
+  private SysIdRoutine m_sysIdRoutineToApply = m_sysIdRoutineRotation;
+
+  
   /**
    * Constructs a CTRE SwerveDrivetrain using the specified constants.
    *
